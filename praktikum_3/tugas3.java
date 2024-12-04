c69c6247631ba9ce335ce0ca6c8c4a9422fb5d87
Eko Muchamad Haryono <66320809+ekomh170@users.noreply.github.com>
2024-12-04 12:09:41 +0700

    Menambahkan Praktikum 3 - Java

---
+++ praktikum_3/tugas3.java
@@ -0,0 +1,15 @@
+public class tugas3 {
+    public static void main(String[] args) {
+        System.out.println("Nama : Eko Muchamad Haryono");
+        System.out.println("NIM : 0110223079");
+        System.out.println("============================");
+        
+        for (int i = 1; i <= 20; i++) {
+            if (i == 13) {
+                continue;
+            } else {
+                System.out.print(i + " ");  
+            }
+        }
+    }
+}
